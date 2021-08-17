9f72abbdf5493e45d8e8f7592fbb47ae47af15d1
Kyle Peck <kyle.peck@west-mec.org>
2021-08-17 12:23:06 -0700

    finished 03

--- React/01-birthday-reminder/src/Components/List.jsx
+++ React/01-birthday-reminder/src/Components/List.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-
-const List = ({ people }) => {
-  return (
-    <>
-      {people.map((person) => {
-        const { id, name, age, image } = person;
-        return (
-          <article key={id} className='person'>
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{age} years</p>
-            </div>
-          </article>
-        );
-      })}
-    </>
-  );
+import React from "react";
+const List = ({ people, remove }) => {
+  return people.map((person) => {
+    const { id, name, age, image } = person;
+    return (
+      <article key={id} className="list">
+        <img src={image} alt={name} />
+        <div style={{ position: "relative" }}>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+          <button
+            style={{
+              width: "6.5rem",
+              position: "absolute",
+              right: 0,
+              bottom: "25%",
+            }}
+            onClick={() => {
+              remove(id);
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      </article>
+    );
+  });
 };
 
 export default List;
